9314a021284bd071e2f7615528f4651cddceeaf2
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-06-01 23:08:05 -0400

    Update admin.py

--- persona/admin.py
+++ persona/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from persona.models import Persona
+from persona.models import Persona,Domicilio
 
 admin.site.register(Persona)
+admin.site.register(Domicilio)
