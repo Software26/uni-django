6e18323735a8249efb5e5525ced2bb3af35b039f
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-06-17 20:41:05 -0400

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django import render
 
 # Create your views here.
 def bienvenido(request):
     return render(request,"bienvenido.html")
 
 def despedida(request):
-    return HttpResponse("<h1>Despedida desde Django</h1>")
-"""
+    return HttpResponse(request,"<h1>Despedida desde Django</h1>")
+
+
 def contacto(request):
-    numeroTelefonico = "Mi numero de contacto es: 809-210-5513 <br>"
-    correo = "Email es: natanaelsosa@gmail.com"  
-    return HttpResponse(numeroTelefonico+correo)
+    numeroTelefonico = ("Mi numero de contacto es: 809-210-5513 <br>")
+    correo = ("Email es: natanaelsosa@gmail.com")  
+    return HttpResponse(request, numeroTelefonico,correo)
     
-    """
